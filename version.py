9a640239b256e910ac14b4b67bad9723065b0517
Takumi Yanagawa <yana@jp.ibm.com>
2025-04-05 14:30:50 +0900

    test

--- version.py
+++ version.py
@@ -1,6 +1,9 @@
 # Dummy Python file to satisfy scanning tools.
 # This file is NOT used for execution or deployment.
-# Required only because the scan tool expects at least one Python file to be present.
+# Required only because the scan tool expects at least one Python file to be present,
+# and at least one third-party package to be imported,
+# otherwise the generated Mend SBOM will contain `components: null`
+# and the subsequent SBOM schema validation will fail.
 
 import yaml
 
