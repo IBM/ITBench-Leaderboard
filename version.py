fb1bb5549b2a276ea53a442f19d39f14c56ff53d
Takumi Yanagawa <yana@jp.ibm.com>
2025-04-05 14:10:56 +0900

    test

---
+++ version.py
@@ -0,0 +1,7 @@
+# Dummy Python file to satisfy scanning tools.
+# This file is NOT used for execution or deployment.
+# Required only because the scan tool expects at least one Python file to be present.
+
+import yaml
+
+yaml.safe_dump({"version": "-"})
